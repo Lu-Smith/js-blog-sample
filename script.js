55d4be7872dc4444f4482542dfb8988e1227f98c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-01 18:06:54 +0100

    Added js to contact updated css of contact page

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ open.addEventListener(`click`, () => container.classList.add(`show-nav`));
 
 close.addEventListener(`click`, () => container.classList.remove(`show-nav`));
 
+/* contact page */
+
+const labels = document.querySelectorAll(`.form-control label`) 
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+       .split(``)
+       .map((letter, idx) => `<span style="transition-delay:${idx * 60}ms">${letter}</span>`)
+       .join(``)
+})
